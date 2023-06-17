f019c88a4a12c348dd18ab3e09846584bdb02dac
CrazyVideoGamer <59106025+CrazyVideoGamer@users.noreply.github.com>
2023-06-17 13:49:24 -0400

    add invalid chrome exec path handling

--- README.md
+++ README.md
@@ -7,7 +7,39 @@ Please note that for protected cloud recordings, the recording *should* have the
 Node.js, npm, and Chrome need to be installed (Firefox is not supported at the moment - might add if someone asks me to, but currently I have no need of adding any such support)
 
 # Installation
-<!-- Run `npm i  -g`, or  -->
+Run `npm i zoom-dl -g`, or, `npx zoom-dl@latest` 
+
+# Usage
+```
+Usage: zoom-dl [options] <url>
+
+Download zoom recordings automatically
+
+Arguments:
+  url                             zoom recording url to download
+
+Options:
+  -V, --version                   output the version number
+  -b, --browser-exec-path <path>
+  -h, --help                      display help for command
+
+C:\Users\lianj\Documents\Coding\zoom-interception-dl>npm start -- -h
+
+> zoom-dl@1.0.0 start
+> node cli.js -h
+
+Usage: zoom-dl [options] <url>
+
+Download zoom recordings automatically
+
+Arguments:
+  url                             zoom recording url to download
+
+Options:
+  -V, --version                   output the version number
+  -b, --browser-exec-path <path>  path to the Chrome executable
+  -h, --help                      display help for command
+```
 
 Simply use through `npm start <zoom-recording-url>`
 
--- cli.js
+++ cli.js
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
 
 const puppeteer = require("puppeteer-core");
-const { program } = require("commander")
+const commander = require("commander");
+const { program } = require("commander");
 const downloadRecording = require('./index');
+const fs = require('fs');
+
+program.exitOverride((err) => {
+  if (err.code.startsWith('commander.missing')) {
+    console.log();
+    program.outputHelp();
+  }
+  process.exit(err.exitCode);
+});
 
 program
   .name('zoom-dl')
   .description('Download zoom recordings automatically')
   .version('1.0.0')
   .argument('<url>', 'zoom recording url to download', parseURL)
-  .requiredOption('-b, --browser-exec-path <path>')
+  .requiredOption('-b, --browser-exec-path <path>', 'path to the Chrome executable', parseChromePath)
   .parse(process.argv);
 
 
@@ -19,18 +29,29 @@ const options = program.opts();
 (async () => {
 
   // must use executablePath so zoom mp4 recording loads (chromium doesn't support mp4)
-  const browser = await puppeteer.launch({ 
-    headless: "new", 
-    executablePath: options.browserExecPath
-  });
-
   try {
-    await downloadRecording(browser, url)
-  } catch (e) {
-    console.error(`${e.name}: ${e.message}`);
+    const browser = await puppeteer.launch({ 
+      headless: "new", 
+      executablePath: options.browserExecPath
+    });
+
+    try {
+      await downloadRecording(browser, url)
+    } catch (e) {
+      console.error(`${e.name}: ${e.message}`);
+      await browser.close()
+      process.exit(1);
+    }
+  } catch(e) {
+    if (e.message === "spawn UNKNOWN") {
+      console.error("Invalid chrome path. Chrome was unable to be launched.")
+      process.exit(1)
+    } else {
+      throw e;
+    }
   }
 
-  await browser.close();
+
 
 })()
 
@@ -41,7 +62,12 @@ function parseURL(value) {
       throw new commander.InvalidArgumentError("Invalid Zoom URL.")
     }
     return value
-  } catch {
-    throw new commander.InvalidArgumentError("Invalid Zoom URL.")
+  } catch { // for the errors caused by new URL(value) (invalid urls, but could be zoom or not)
+    throw new commander.InvalidArgumentError("Invalid URL.")
   }
+}
+
+function parseChromePath(value) {
+  if (!fs.existsSync(value)) throw new commander.InvalidArgumentError("Path does not exist.");
+  return value
 }
